c89a55565d0cdfe15deaf6de5471c86192a4682c
Hao Zhou <hzhou129527@gmail.com>
2024-08-13 22:56:40 -0400

    08132024\

--- main.js
+++ main.js
@@ -1,21 +1,62 @@
 import { resources } from "./src/Resource";
+import { Sprite } from "./src/Sprite";
+import { Vector2 } from "./src/Vector2";
 import "./style.css";
 
 const canvas = document.querySelector("#game-canvas");
 const ctx = canvas.getContext("2d");
 
-const draw = () => {
-  const sky = resources.images.sky;
-  if (sky.isLoaded) {
-    ctx.drawImage(sky.image, 0, 0);
-  }
+// const draw = () => {
+//   const sky = resources.images.sky;
+//   if (sky.isLoaded) {
+//     ctx.drawImage(sky.image, 0, 0);
+//   }
 
-  const ground = resources.images.ground;
-  if (ground.isLoaded) {
-    ctx.drawImage(ground.image, 0, 0);
-  }
+// const draw = () => {
+//   const sky = resources.images.sky;
+//   if (sky.isLoaded) {
+//     ctx.drawImage(sky.image, 0, 0);
+//   }
+
+//   const ground = resources.images.ground;
+//   if (ground.isLoaded) {
+//     ctx.drawImage(ground.image, 0, 0);
+//   }
+// };
+
+// const hero = new Sprite({
+//   resource: resources.images.hero,
+//   hFrames: 3,
+//   vFrames: 8,
+//   frame: 1,
+// });
+
+const skySprit = new Sprite({
+  resource: resources.images.sky,
+  frameSize: new Vector2(320, 180),
+});
+
+const ground = new Sprite({
+  resource: resources.images.ground,
+  frameSize: new Vector2(320, 180),
+});
+
+const hero = new Sprite({
+  resource: resources.images.hero,
+  frameSize: new Vector2(32, 32),
+  hFrames: 3,
+  vFrames: 8,
+  frame: 1,
+});
+
+const heroPos = new Vector2(16 * 5, 16 * 5);
+
+const draw = () => {
+  skySprit.drawImage(ctx, 0, 0);
+  ground.drawImage(ctx, 0, 0);
+  hero.drawImage(ctx, heroPos.x, heroPos.y);
 };
 
-// setInterval(() => {
-//   draw();
-// }, 300);
+setTimeout(() => {
+  draw();
+}, 300);
